b0125c27b816ef2d67e2cee59ae40c7e4f8aa6a7
Param Jani <69115040+paramjani12@users.noreply.github.com>
2022-04-17 09:54:35 +0530

    Update navbar.js

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -7,6 +7,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -55,7 +57,15 @@ const Navbar = () => {
         <Box  sx={{ flexGrow: 1 }}>
             <AppBar style={{backgroundColor:"#006D77"}} position="static">
                 <Toolbar>
-                
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  sx={{ mr: 2 }}
+                >
+            <MenuIcon />
+          </IconButton>
                 <Typography
                     variant="h6"
                     noWrap
